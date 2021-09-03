7e4a68d2ed9658ba8ad24b50b335a7ca2b36e21e
Kumar Sagar <geekslazy@gmail.com>
2021-09-03 10:17:33 +0530

    created a text file

---
+++ index.js
@@ -0,0 +1,18 @@
+const scrape = require('website-scraper'); 
+const websiteUrl = 'https://azyrusthemes.com/sharehub/demo/'; 
+ 
+scrape({ 
+    urls: [websiteUrl], 
+    urlFilter: function (url) { 
+        return url.indexOf(websiteUrl) === 0; 
+    }, 
+    recursive: true, 
+    maxDepth: 50, 
+    prettifyUrls: true, 
+    filenameGenerator: 'bySiteStructure', 
+    directory: '/Desktop/script' 
+}).then((data) => { 
+    console.log("Entire website succesfully downloaded"); 
+}).catch((err) => { 
+    console.log("An error ocurred", err); 
+}); 
